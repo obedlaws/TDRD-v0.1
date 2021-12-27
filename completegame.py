936777c3f7183d9057c13ec49b80efe352ba2494
obedlaws <92595276+obedlaws@users.noreply.github.com>
2021-12-27 18:16:33 -0500

    test v0.0.1

---
+++ completegame.py
@@ -0,0 +1,11 @@
+answer = input("""
+||||||||||||||||||||||||||||||||||||||||||||||||
+|||     Welcome to the Forgotten Forest      |||
+||||||||||||||||||||||||||||||||||||||||||||||||
+        Ready to survive the night? (Y/N)?
+""" )
+
+if answer.lower().strip() == "y"
+
+else:
+    print("Ok, see you soon")
