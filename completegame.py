48fbbba8a69beb631b05d08f2b01e21540a86cd1
obedlaws <92595276+obedlaws@users.noreply.github.com>
2021-12-27 18:39:21 -0500

    Update completegame.py

--- completegame.py
+++ completegame.py
@@ -7,5 +7,14 @@ answer = input("""
 
 if answer.lower().strip() == "y":
 
-else:
-    print("Ok, see you soon")
+        answer = input("""
+        [Pick you option by writing a number]
+        You wake with a big headache and everything is in flames around you. You look around and see a gun, a backpack and a knife. What do you do?
+                        [1. Pick items up.  2. Start Exploring Around]                      
+        """)
+
+        if answer == "1"
+
+
+        else:
+                print("Ok, see you soon")
