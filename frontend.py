cb3430ceb168569b7e3071ce5789674fcd268832
obedlaws <92595276+obedlaws@users.noreply.github.com>
2021-12-28 15:19:42 -0500

    simple dice version v0.1

--- frontend.py
+++ frontend.py
@@ -8,15 +8,33 @@ TTTTTTT    DDDDD       RRRRRR     DDDDD
   TTT      DD    DD    R  R       DD     DD
   TTT      DD   DD     R   R      DD   DD
   TTT      DDDDD       R    R     DDDDD
-           Treminal DnD Random Dice          
+        Treminal DnD Random Dice v0.1   
 |||||||||||||||||||||||||||||||||||||||||||||
-     Ready to start, Hero!? (y/n)?
-""" )
+           Simple Dice Version
 
+        Ready to start, Hero!? (y/n)?
+""" )
 if answer.lower().strip() == "y":
+        
         answer = input("""
-     1. Create New Character  2. Use Dice 3. Exit
+      ///CHOOSE THE DICE YOU ARE LOOKIN FOR////
+
+   1. D20   2. D12   3. D10   4. D8   5. D6   6. D4
+
         """ )
+        if answer == "1":
+                answer = backendfunctions.dtwenty
+        if answer == "2":
+                answer = print("YES")
+        if answer == "3":
+                answer = print("YES")
+        if answer == "4":
+                answer = print("YES")
+        if answer == "5":
+                answer = print("YES")
+        if answer == "6":
+                answer = print("YES")
+                
 
 else:
         print("Ok, see you soon")
