9d8bc74b29c484608417b95b23128daa90042a00
obedlaws <92595276+obedlaws@users.noreply.github.com>
2021-12-27 19:27:14 -0500

    Update completegame.py

--- completegame.py
+++ completegame.py
@@ -13,8 +13,23 @@ if answer.lower().strip() == "y":
                         [1. Pick items up.  2. Start Exploring Around]                      
         """)
 
-        if answer == "1"
+                if answer == "1":
+                        answer = input(("""
+                You check the gun and see that have one magazine in it. Then you check the bag and see that
+                there is just one more magazine in it and some meds just in case anything goes wrong and also
+                a power bar, 'Nice!' - you say to your self. Atlast you pick up the knife and get ready to
+                get into the forest in front of you.
+                                [1. Go Next Slide]                      
+                """))
 
+                elif answer == "2":
+                        answer = input("""
+                You check the gun and see that have one magazine in it. Then you check the bag and see that
+                there is just one more magazine in it and some meds just in case anything goes wrong and also
+                a power bar, 'Nice!' - you say to your self. Atlast you pick up the knife and get ready to
+                get into the forest in front of you.
+                                [1. Go Next Slide]                      
+                """)
 
         else:
                 print("Ok, see you soon")
