211d258e67b743041fb1f3208b9b8b32168235dd
obedlaws <92595276+obedlaws@users.noreply.github.com>
2021-12-28 11:08:14 -0500

    Change of Theme TDRD v0.1

--- completegame.py
+++ completegame.py
@@ -1,36 +1,24 @@
+import ranfunctions
+
 answer = input("""
-||||||||||||||||||||||||||||||||||||||||||||||||
-|||     Welcome to the Forgotten Forest      |||
-||||||||||||||||||||||||||||||||||||||||||||||||
-        Ready to survive the night? (Y/N)?
+|||||||||||||||||||||||||||||||||||||||||||||
+TTTTTTT    DDDDD       RRRRRR     DDDDD
+  TTT      DD   DD     R     R    DD   DD
+  TTT      DD    DD    RRRRRR     DD     DD
+  TTT      DD    DD    R  R       DD     DD
+  TTT      DD   DD     R   R      DD   DD
+  TTT      DDDDD       R    R     DDDDD
+           Treminal DnD Random Dice          
+|||||||||||||||||||||||||||||||||||||||||||||
+     Ready to create your Hero!? (y/n)?
 """ )
 
 if answer.lower().strip() == "y":
-
         answer = input("""
-        [Pick you option by writing a number]
-        You wake with a big headache and everything is in flames around you. You look around and see a gun, a backpack and a knife. What do you do?
-                        [1. Pick items up.  2. Start Exploring Around]                      
-        """)
+     1. Create New Character  2. Use Dice 3. Exit
+        """ )
 
-        if answer == "1":
-                answer = input(("""
-                You check the gun and see that have one magazine in it. Then you check the bag and see that
-                there is just one more magazine in it and some meds just in case anything goes wrong and also
-                a power bar, 'Nice!' - you say to your self. Atlast you pick up the knife and get ready to
-                get into the forest in front of you.
-                                [1. Go Next Slide]                      
-                """))
-                if answer == "1":
-                        answer == input("")
 
-        elif answer == "2":
-                answer = input("""
-                You walk up into a crossroad that seems odly crated by nature. You see two
-                road leading into the forest. From the right side you can hear a river and  from the left side
-                road you can see a dark tunner created by 
-                                [1. Go Right  2. Go Left  3. Get back and pick up the items]                      
-                """)
 
-        else:
-                print("Ok, see you soon")
+else:
+        print("Ok, see you soon")
